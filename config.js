669e8ea2367628da7749f800f79bf88f7e4ae5e1
VishnuDath710 <vishnudathrock@gmail.com>
2024-03-26 15:32:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 // Define base URL
-const BASE_URL = "http://teleuniv.in/netra/studentQR/";
+const BASE_URL = "https://images.weserv.nl/?url=http://teleuniv.in/netra/studentQR/";
+const IMG_URL = "https://images.weserv.nl/?url=http://teleuniv.in/sanjaya/student-images/";
